d2002fd866a50ded3ea1526e9341ab033c93273b
Seiya <liyang215601@outlook.com>
2018-12-07 00:24:12 +0800

    201812070023 add find public common word in string

--- Learning Note/kill_by_runningtime.sh
+++ Learning Note/kill_by_runningtime.sh
@@ -19,41 +19,16 @@ find /home/  -mtime 1 -o -mmin 1
 
 p_name=ffmpeg
 
-# while true
-
-# do
-
-# 暂停60s
-
-# sleep 60
-
-# 取进程ID号
-
-# list="$(ps -A | grep $p_name|grep -v 3537|sort -k3,3| head -n3 |awk '{print $1}')"
 list="$(ps -A | grep $p_name|sort -k3,3|awk '{print $1}')"
 
- 
-
-# 循环取时间，并kill超长进程
-
 for pid in $list
-
 do
-
-	time="$(ps -eo pid,etime  | grep $pid | awk '{print $2}'|./script.awk)"
-
-	echo $pid $time
-
-	#if [[ $time -ge '10800'  ]];then
-
-	# kill -9 $pid
-	# echo $pid $time
-
-	#fi
-
+  time="$(ps -eo pid,etime |grep $pid|awk '{print $2}'|./script.awk)"
+  if [[ $time -ge '10800' ]];then
+    echo $pid $time
+    kill -9 $pid
+  fi
 done
-
-# done
 
 
 #!/usr/bin/awk -f  
@@ -83,3 +58,77 @@ BEGIN { FS = ":" }
   }
 
 }
+
+1.判断变量
+
+复制代码 代码如下:
+
+read -p "input a word :" word
+if  [ ! -n "$word" ] ;then
+    echo "you have not input a word!"
+else
+    echo "the word you input is $word"
+fi
+2.判断输入参数
+
+复制代码 代码如下:
+
+#!/bin/bash
+if [ ! -n "$1" ] ;then
+    echo "you have not input a word!"
+else
+    echo "the word you input is $1"
+fi
+以下未验证。
+
+3. 直接通过变量判断
+
+如下所示:得到的结果为: IS NULL
+复制代码 代码如下:
+
+#!/bin/sh
+para1=
+if [ ! $para1 ]; then
+  echo "IS NULL"
+else
+  echo "NOT NULL"
+fi 
+4. 使用test判断
+
+得到的结果就是: dmin is not set! 
+复制代码 代码如下:
+
+#!/bin/sh
+dmin=
+if test -z "$dmin"
+then
+  echo "dmin is not set!"
+else  
+  echo "dmin is set !"
+fi
+5. 使用""判断
+
+复制代码 代码如下:
+
+#!/bin/sh 
+dmin=
+if [ "$dmin" = "" ]
+then
+  echo "dmin is not set!"
+else  
+  echo "dmin is set !"
+fi
+下面是我在某项目中写的一点脚本代码， 用在系统启动时:
+复制代码 代码如下:
+
+#! /bin/bash
+echo "Input Param Is [$1]"
+
+if [ ! -n "$1" ] ;then
+ echo "you have not input a null word!"
+ ./app1;./app12;./app123
+elif [ $1 -eq 2 ];then
+ ./app12;./app123
+elif [ $1 -eq 90 ];then
+ echo "yy";
+fi
