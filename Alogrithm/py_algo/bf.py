2871303a833071fd55cb23035988880b6c3cce4c
Seiya <liyang215601@outlook.com>
2019-01-02 15:10:14 +0800

    201901021509 add rk algo in python

--- Alogrithm/py_algo/bf.py
+++ Alogrithm/py_algo/bf.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 # -*- Author:Seiya -*-
+import hashlib
+
+
+def hash_(str):
+    hashobj = hashlib.md5()
+    hashobj.update(str)
+    digest = hashobj.hexdigest()
+    return digest
 
 
 def bf(str, substr):
@@ -26,8 +34,25 @@ def bf(str, substr):
             i = i + 1
 
 
-str = list("asdkfhkshdfbbakshdfk")
-substr = list("bba")
-print(str)
-print(substr)
-bf(str, substr)
+def rk(str, substr):
+    """BF利用哈希算法优化"""
+    substr_len = len(substr)
+    substr_count = len(str) - substr_len + 1
+    hash_list = list()
+    count = 0
+    while substr_count:
+        hash_list.append(hash_((str[count:count+substr_len]).encode("utf-8")))
+        substr_count -= 1
+        count += 1
+    if hash_(substr.encode("utf-8")) in hash_list:
+        print("RK匹配成功")
+    else:
+        print("RK匹配失败")
+
+
+str = "asdkfhkshdfbbakshdfk"
+substr = "bba"
+# print(str)
+# print(substr)
+# bf(str, substr)
+rk(str, substr)
