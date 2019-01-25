b9ffb18c0adda0901e2a79b89c6d407e8ce683b3
Seiya <liyang215601@outlook.com>
2019-01-25 23:17:23 +0800

    201901252317 fix mergesort bug

--- Alogrithm/py_algo/base/mergesort.py
+++ Alogrithm/py_algo/base/mergesort.py
@@ -2,39 +2,40 @@
 # -*- encoding: utf-8 -*-
 # -*- Author:Seiya -*-
 import random
+import sys
+
+sys.setrecursionlimit(10000)
 
 
-def merge(src, temp, start, mid, end):
-    i = start
-    j = mid
-    t = start
-    while i != mid + 1 and j != end:
-        if src[i] < src[j]:
-            temp[t] = src[i]
-            t, i = t + 1, i + 1
+def merge(data, start, mid, end):
+    left_set_num = mid - start + 1
+    right_set_num = end - mid
+    left = list()
+    right = list()
+    for i in range(left_set_num):
+        left.append(data[start+i-1])
+
+    for j in range(right_set_num):
+        right.append(data[mid+j])
+
+    i = 0
+    j = 0
+    k = start
+    while k <= end:
+        if left[i] <= right[j]:
+            data[k] = left[i]
+            i = i + 1
         else:
-            temp[t] = src[j]
-            t, j = t+1, j+1
-    while i != mid:
-        temp[t] = src[i]
-        t, i = t + 1, i + 1
-    while j != end:
-        temp[t] = src[j]
-        t, j = t + 1, j + 1
-    i = start
-    while i < end:
-        src[i] = temp[i]
-        i += 1
+            data[k] = right[j]
+            j = j + 1
 
 
-def mergesort(src, temp, start, end):
-    if start > end:
-        return None
-    else:
-        mid = int((0 + len(data_set)) / 2)
-        mergesort(src, temp, start, mid)
-        mergesort(src, temp, mid+1, end)
-        merge(src, temp, start, mid, end)
+def mergesort(src, start, end):
+    if start < end:
+        mid = int((start + end) / 2)
+        mergesort(src, start, mid)
+        mergesort(src, mid+1, end)
+        merge(src, start, mid, end)
 
 
 data_set = list()
@@ -42,8 +43,7 @@ for item in range(10):
     data_set.append(item)
 
 random.shuffle(data_set)
-temp = list()
-print(mergesort(data_set, temp, 0, len(data_set)))
+print(mergesort(data_set, 0, len(data_set)))
 # data_set = [5, 6, 7, 8, 9, 0, 1, 2, 3, 4]
 # print(data_set)
 # temp = [0]*len(data_set)
