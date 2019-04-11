e731b5a6cde1636a5bde5c0f144d4fceab673a6c
Administrator <admin@example.com>
2019-04-11 16:08:24 +0800

    201904111607 fix py_algo kmp

--- Alogrithm/py_algo/base/kmp.py
+++ Alogrithm/py_algo/base/kmp.py
@@ -4,16 +4,23 @@
 
 
 def gen_next(str):
-    i, k, m = 0, -1, len(str)
-    pnext = [-1] * m
-    while i < m-1:
-        # a, b = str[i], str[k]
-        if k == -1 or str[i] == str[k]:
-            i, k = i+1, k+1
-            pnext[i] = k
-        else:
-            k = pnext[k]
-    return pnext
+    length = len(str)
+    str_list = list(str)
+    slow = -1
+    next = [None] * length
+    next[0] = -1
+    quick = 1
+    while quick < length:
+        while slow > -1 and str_list[slow + 1] != str_list[quick]:
+            slow = next[slow]
+
+        if str_list[slow + 1] == str_list[quick]:
+            slow += 1
+
+        next[quick] = slow
+        quick += 1
+
+    return next
 
 
 print(gen_next("abbcabcaabbcaa"))
